53f85f588abf09dbdfae6ee4e5f6f0c024bab717
Prathamesh Pawar <163292716+PrathameshSPawar@users.noreply.github.com>
2025-04-28 22:52:41 +0530

    Create isPrime_No.java

---
+++ isPrime_No.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main 
+{
+    
+    public static boolean isPrime(int n)
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner pp=new Scanner(System.in);
+        System.out.println("Enter the number that you want to check: ");
+        int n=pp.nextInt();
+        System.out.println(isPrime(n));
+    }
+}
