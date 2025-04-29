e22c75888200c6e0b535fd127d419f465b2c3655
Prathamesh Pawar <163292716+PrathameshSPawar@users.noreply.github.com>
2025-04-29 17:28:40 +0530

    Update Divisible_SubSet_PigeanP.java

--- Divisible_SubSet_PigeanP.java
+++ Divisible_SubSet_PigeanP.java
@@ -1,60 +1,52 @@
-//https://www.codechef.com/problems/DIVSUBS?tab=statement
+https://www.codechef.com/problems/DIVSUBS
+/*here their are two ways to solve this problem one is using HashMap but which gives n logn TC 
+Second one is using arrays which gives O(n) TC*/
 import java.util.*;
-import java.lang.*;
-import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner pp = new Scanner(System.in);
+        System.out.println("Enter the Test cases: ");
+        int t=pp.nextInt();
+        while(t-->0)
+        {
+            int n=pp.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i]=pp.nextInt();
+            }
 
-public class Main
-{
-	public static void main(String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner pp=new Scanner(System.in);
-	    int t=pp.nextInt();
-	
-	    while(t-->0)
-	    {
-	        int n=pp.nextInt();
-	        int arr[]=new int[n];
-	        for(int i=0;i<n;i++) arr[i]=pp.nextInt();
-	    
-	        int prefSum[]=new int[n];
-	        prefSum[0]=arr[0];
-	        for(int i=1;i<n;i++) prefSum[i]=prefSum[i-1]+arr[i];
-	    
-	        for(int i=0;i<n;i++) prefSum[i]=prefSum[i]%n;
-	    
-	        int start=0,end=0;
-	        //HashMap<Integer,Integer> mp=new HashMap<>();
-		    //instead of using map which will give log n complexity we can use list where all initialize with -1 at statring time
-		List<Integer> list=new ArrayList<>(n,-1);
-	        for(int i=0;i<n;i++)
-	        {
-	            //case 1 where prefSum=0
-	            if(prefSum[i]==0)
-	            {
-	                start=0;
-	                end=i;
-	                break;
-	            }else{
-	                int rem=prefSum[i];
-	                //if(mp.containsKey(rem))
-			if(list.get(rem))
-	                {
-	                    start=list.get(rem)+1;
-	                    end=i;
-	                    break;
-	                }
-	                mp.put(rem,i);
-	            }
-	        }
-	        System.out.print(end-start+1);
-	        System.out.println();
-	        for(int i=start;i<=end;i++)
-	        {
-	            System.out.print(i+1+ " ");
-	        }
-	        System.out.println();
-	    }
+            int pref_sum[]=new int[n];
+            pref_sum[0]=arr[0];
+            for(int i=1;i<n;i++)  pref_sum[i]=pref_sum[i-1]+arr[i];
 
-	}
+            for (int i=0;i<n;i++) pref_sum[i]=pref_sum[i]%n;
+
+            int start=0,end=0;
+            HashMap<Integer,Integer> map=new HashMap<>();
+            int isPresent[]=new int[n];
+            Arrays.fill(isPresent,-1);
+            for(int i=0;i<n;i++)
+            {
+                int curr=pref_sum[i];
+                if(curr==0){
+                    start=0;
+                    end=i;
+                    break;
+                }else{
+                    if(isPresent[curr]!=-1)
+                    {
+                        start=isPresent[curr]+1;
+                        end=i;
+                        break;
+                    }
+                    isPresent[curr]=i;
+                }
+            }
+            for(int i=start;i<end+1;i++)
+            {
+                System.out.println(i+ " ");
+            }
+        }
+    }
 }
